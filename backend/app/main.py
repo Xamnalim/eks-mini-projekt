7f0d46e501318eeecc89689caa581812669140b3
Maksymilian Łapiński <mlapinski98@gmail.com>
2022-01-23 15:27:50 +0100

    DB connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+import os
+import time
 
+import psycopg2
+from fastapi import FastAPI
+from psycopg2.extras import RealDictCursor
 
 app = FastAPI()
+
+
+# Database connection
+while True:
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connection to database failed!\n", error)
+        time.sleep(3)
 
 
 @app.get("/")
